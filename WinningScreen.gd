5e89597545c9c7b7fc17e6d84fab8dc5d0cfb73b
NiklasSWT <73588535+NiklasSWT@users.noreply.github.com>
2022-07-06 14:18:11 +0200

    Add files via upload

---
+++ WinningScreen.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var scene = 0;
+
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://Menu.tscn")
+
+
+func _on_WeiterButton_pressed():
+	if (scene == 0):
+		get_tree().change_scene("res://Dijkstra1.tscn");
+		scene = 1;
+	else:
+		get_tree().change_scene("res://Menu.tscn");
+		scene = 0;
